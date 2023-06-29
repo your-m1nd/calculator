97b9f20cbe61adbdfeb011964f744e7db051108e
your-m1nd <vi.neurogirl@gmail.com>
2023-06-29 22:01:48 +0300

    add changes

--- form.js
+++ form.js
@@ -35,17 +35,16 @@ function checkAge(number) {
     }
 }
 
-//проверка поля Пол (не работает)
+//проверка поля Пол 
    function checkGender() {
-    let inputGender = document.querySelector('input[name="gender"]');
-    if (inputGender.checked) {
-
-    }
-    else {
+    let inputGender = document.getElementsByName('gender');
+      if ((inputGender[0].checked == false) &&
+      (inputGender[1].checked == false))  {
+        valid = false;
         let spanName4 = document.getElementById('spanName4');
         spanName4.outerHTML = 'Пол не выбран';
-    }
-   }
+      }
+      }
     
 //проверка Профессии
 
@@ -81,52 +80,17 @@ else {
 }
 
 
-//let errors = [];
-
-//function checkValidity(input) {
-   // let validity = input.validity;
-    
-    //if (validity.valueMissing) {
-      //  errors.push('Поле ' + input.placeholder + ' не заполнено'); }
-        
-    //if (validity.patternMismatch) {
-      //  errors.push('Неверный формат заполнения');}
-    //if (validity.rangeOverflow) {
-    //    let max = getAttributeValue(input, 'max');
-     //   errors.push('Максимальное значение не может быть больше, чем ' + max);
-    //}
-    //if (validity.rangeUnderflow) {
-    //    let min = getAttributeValue(input, 'min');
-     //   errors.push('Минимальное значение не может быть больше, чем ' + min);
-   // }
-   //     }
-
-//function checkAll() {
-   // errors = [];
-   // let inputs = document.querySelectorAll('input');
-
-    //for (let input of inputs) {
-    //    checkValidity(input);
-    //}
-
-   //document.getElementById('errorsInfo').innerHTML = errors.join('. <br>');
-//}
-
-
 //пока не стоит галочка, нельзя нажать на кнопку отправки формы
+let submit = document.getElementById('submit');
 
 function check() {
-    var submit = document.getElementsByName('submit')[0];
     if (document.getElementById('politics').checked)
     submit.disabled = '';
     else
     submit.disabled = 'disabled';
     }
 
-let form = document.forms.form;
-let button = document.getElementsByTagName('button');
-
-form.addEventListener('submit', function (evt) {
+submit.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
     form.reset();
